ea4f06568826411b867b405f75d52e18b0208132
Ciro Rey <cirorey95@gmail.com>
2024-10-09 17:19:57 -0300

    change size component/Hero and botton-name

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,10 @@
 export default function Hero() {
     return (
-        <section className="bg-gray-900 text-white h-screen flex flex-col justify-center items-center text-center">
-            <h1 className="text-5xl font-bold mb-4">¡Hola, soy [Tu Nombre]!</h1>
-            <p className="text-lg mb-8">Desarrollador Frontend apasionado por construir experiencias increíbles.</p>
+        <section className="bg-gray-900 text-white h-80 flex flex-col justify-center items-center text-center px-4">
+            <h1 className="text-4xl md:text-5xl font-bold mb-2">¡Hola, soy Ciro Rey!</h1>
+            <p className="text-base md:text-lg mb-4">FullStack Developer, apasionado por construir experiencias increíbles.</p>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Descubre más
+                Proyectos
             </button>
         </section>
     );

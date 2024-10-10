fb04b38a5353237cc0b023062e0b2d6972a4b74d
Ciro Rey <cirorey95@gmail.com>
2024-10-10 15:56:04 -0300

    add href to botton

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,15 @@
+import Link from 'next/link';
+
 export default function Hero() {
     return (
         <section className="bg-gray-900 text-white h-80 flex flex-col justify-center items-center text-center px-4">
             <h1 className="text-4xl md:text-5xl font-bold mb-2">¡Hola, soy Ciro Rey!</h1>
             <p className="text-base md:text-lg mb-4">FullStack Developer, apasionado por construir experiencias increíbles.</p>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Proyectos
-            </button>
+            <Link href="#projects" passHref>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Proyectos
+                </button>
+            </Link>
         </section>
     );
 }

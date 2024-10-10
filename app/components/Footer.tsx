8eadd6d3ac2a6ff220c9592d252eaec9dc41aa99
Ciro Rey <cirorey95@gmail.com>
2024-10-10 15:56:46 -0300

    add name in <p/>

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="bg-gray-800 text-white text-center py-4">
-            <p>© 2024 TuNombre. Todos los derechos reservados.</p>
+            <p>© 2024 Ciro Rey. Todos los derechos reservados.</p>
         </footer>
     );
 }

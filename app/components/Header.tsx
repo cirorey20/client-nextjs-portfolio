23d2856fbbbd9332af6298b4ca823e0c56fbcc40
Ciro Rey <cirorey95@gmail.com>
2024-10-10 15:54:40 -0300

    Change name logo (C-Dev-DJR)

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="bg-gray-800 text-white p-4">
       <div className="container mx-auto max-w-5xl flex flex-col md:flex-row justify-between items-center">
         {/* Logo */}
-        <div className="text-lg font-bold mb-4 md:mb-0">MiLogo</div>
+        <div className="text-lg font-bold mb-4 md:mb-0">C-Dev-JR</div>
         
         {/* Navbar */}
         <nav>

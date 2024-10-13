a6385a47be5225ed359f0371eb34618663041e2d
Ciro Rey <cirorey95@gmail.com>
2024-10-13 17:57:29 -0300

    test1

--- app/api/tests/route.ts
+++ app/api/tests/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from 'next/server';
 
-export async function POST(request: Request) {
-  const { email, subject, message } = await request.json();
-
-  try {
-    console.log(email, subject, message)
-    return NextResponse.json(email);
-  } catch (error) {
-    console.error('Error al obtener los datos:', error);
-    return NextResponse.json({ error: 'Error al obtener los datos' }, { status: 500 });
-  }
+export async function GET() {
+    try {
+        // const response = await fetch('https://rickandmortyapi.com/api/character/');
+        const response = await fetch('https://api-googlesheets-portfolio.vercel.app/api/v1/users');
+        const data = await response.json();
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error('Error al obtener los datos:', error);
+        return NextResponse.json({ error: 'Error al obtener los datos' }, { status: 500 });
+    }
 }

b453e7df25c4bd9555c35a4e9b9f8cdf86a0f334
Ciro Rey <cirorey95@gmail.com>
2024-10-14 16:11:06 -0300

    add cache:no-store in fetch the api/posts and api/users

--- app/api/googleSheets/users/route.ts
+++ app/api/googleSheets/users/route.ts
@@ -3,7 +3,9 @@ import { NextResponse } from 'next/server';
 export async function GET() {
     try {
         // const response = await fetch('https://rickandmortyapi.com/api/character/');
-        const response = await fetch('https://api-googlesheets-portfolio.vercel.app/api/v1/users');
+        const response = await fetch('https://api-googlesheets-portfolio.vercel.app/api/v1/users', {
+            cache: 'no-store'
+        });
         const data = await response.json();
         return NextResponse.json(data);
     } catch (error) {
